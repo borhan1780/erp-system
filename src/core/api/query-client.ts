c2269270c3d7e9b8f834485f1ecca8dcaa78bcf8
Borhan-Hashemi <hashemiborhan00@gmail.com>
2026-08-10 12:54:16 +0330

    perf(query): optimize React Query caching and refetching

--- src/core/api/query-client.ts
+++ src/core/api/query-client.ts
@@ -3,11 +3,12 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 0,
-      gcTime: 0,
-      refetchOnMount: "always",
+      staleTime: 1000 * 60 * 15, // دیتا به مدت ۱۵ دقیقه تازه مانده و از Cache خوانده می‌شود
+      gcTime: 1000 * 60 * 60, // ماندگاری دیتای کش‌شده در حافظه به مدت ۱ ساعت
+      refetchOnMount: false, // جلوگیری از ارسال درخواست مجدد هنگام بازگشت به صفحه
       refetchOnWindowFocus: false,
       refetchOnReconnect: true,
+      retry: 1,
     },
   },
 });
