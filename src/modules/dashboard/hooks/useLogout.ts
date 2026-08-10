aa97836ca1c2c2b60f06bde87142351ea8ec41b3
Borhan-Hashemi <hashemiborhan00@gmail.com>
2026-08-10 12:55:41 +0330

    update:logout custom hook

--- src/modules/dashboard/hooks/useLogout.ts
+++ src/modules/dashboard/hooks/useLogout.ts
@@ -1,9 +1,25 @@
-import { useMutation } from "@tanstack/react-query";
-
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 import { logout } from "../api/logout.api";
+import { authStorage } from "@/core/security";
 
 export function useLogout() {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: logout,
+    onSettled: () => {
+      authStorage.clear();
+
+      localStorage.removeItem("session_company_id");
+      localStorage.removeItem("session_ledger_id");
+      localStorage.removeItem("session_period_id");
+      localStorage.removeItem("session_module_id");
+
+      queryClient.clear();
+
+      navigate("/", { replace: true });
+    },
   });
 }
